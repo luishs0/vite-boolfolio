0290dc1638343c679618e50308f76eaf2b88e771
luishs0 <luishs.doc@gmail.com>
2023-01-19 16:38:38 +0100

    Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <script>
 
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      baseUrl: 'http://127.0.0.1:8000',
+      projects: [],
+      loading: false
+    }
+  },
+  created() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get(`${this.baseUrl}/api/projects`).then(resp => { this.projects = resp.data.results; console.log(this.projects); })
+    },
+  },
+
+}
 </script>
 
 
 <template>
   <h1>Ciao</h1>
+
 </template>
 
 
 <style lang="scss">
-
+@use "./styles/general.scss" as *;
 </style>
